39f50bcd5f3df6705a7fa48b581e94a646f0b608
Li Boyan <lbytony@126.com>
2020-10-10 11:18:59 +0800

    Finish Excel Output

---
+++ cloudfunctions/genExcelFile/index.js
@@ -0,0 +1,53 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const nodeExcel = require('node-xlsx');
+const path = require('path');
+
+cloud.init({
+  env: 'x1-vgiba'
+})
+const act = cloud.database().collection('activity');
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    var targetData = event.dataList;
+    var aid = event.aid;
+    var actName;
+    await act.doc(aid).get().then(
+      res => {
+        // res.data 包含该记录的数据
+        actName = res.data.title
+        console.log(actName)
+      }
+    )
+    let excelFileName = actName + '-' + parseInt(Date.now() / 10000) + "-export.xlsx"
+    let alldata = [];
+    let row = ["序号", "姓名", "学号", "学院", "电子邮箱", "手机号"]; //表属性
+    alldata.push(row);
+    console.log('data:', targetData);
+    // 添加每一行数据
+    for (let key in targetData) {
+      let arr = [];
+      arr.push(parseInt(key)+1);
+      arr.push(targetData[key].name);
+      arr.push(targetData[key].uid);
+      arr.push(targetData[key].dep1);
+      arr.push(targetData[key].email);
+      arr.push(targetData[key].tel);
+      alldata.push(arr)
+    }
+    //3，把数据保存到excel里
+    var buffer = nodeExcel.build([{
+      name: "Sheet1",
+      data: alldata
+    }]);
+    //4，把excel文件保存到云存储里
+    return await cloud.uploadFile({
+      cloudPath: 'outputExcels/' + excelFileName,
+      fileContent: buffer, //excel二进制文件
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
--- miniprogram/packageB/regDetails/regDetails.js
+++ miniprogram/packageB/regDetails/regDetails.js
@@ -1,4 +1,3 @@
-
 var util = require('../../utils/util.js')
 
 wx.cloud.init({
@@ -11,50 +10,52 @@ const register = db.collection('register')
 
 Page({
   data: {
-
-    activeNames:['1'],
-
+    aid: '',
+    activeNames: ['1'],
     reg_number: '',
     max_number: '',
     regStu: [],
     searchValue: '',
+    tempFileURL: '',
+    fileUrl: ''
   },
 
   onLoad: function (options) {
     var that = this
     let actID = options.aid
+    this.setData({
+      aid: actID
+    })
     //加载报名人数 和列表
-    register.where({ 
+    register.where({
       aid: actID
     }).get({
       success: res => {
         that.setData({
-          reg_number : res.data.length
+          reg_number: res.data.length
         })
         res.data.map(active => {
           console.log(active._openid)
           db.collection('user').where({
-              _openid: active._openid
-            })
-            .get({
-              success: res => {
-                that.setData({
-                  regStu: [...that.data.regStu, ...res.data],
-                })
-              },
-              fail(res) {
-                console.log("fail", res)
-              }
-            })
+            _openid: active._openid
+          }).get({
+            success: res => {
+              that.setData({
+                regStu: [...that.data.regStu, ...res.data],
+              })
+            },
+            fail: res => {
+              console.log("fail", res)
+            }
+          })
         })
       },
-      fail(res) {
+      fail: res => {
         console.log("fail", res)
       }
-    }),
-
-    //加载人数上限
-    db.collection('activity').where({ 
+    })
+    // 加载人数上限
+    db.collection('activity').where({
       _id: actID
     }).get({
       success: res => {
@@ -67,15 +68,12 @@ Page({
       }
     })
   },
-
-
   onChangeUser(event) {
-    console.log("twet",event)
+    console.log("twet", event)
     this.setData({
       activeNames: event.detail,
     });
   },
-
   onChange(event) {
     this.setData({
       activeNames: event.detail,
@@ -87,7 +85,68 @@ Page({
   onClose(event) {
     Toast(`关闭: ${event.detail}`);
   },
-  onSearch(){
+  onSearch() {
     console.log(1)
+  },
+  onClickGenExcel() {
+    wx.showLoading({
+      title: '正在导出...',
+    });
+    console.log(this.data.regStu);
+    let that = this;
+    that.saveExcel();
+  },
+  saveExcel() {
+    let that = this
+    wx.cloud.callFunction({
+      name: "genExcelFile",
+      data: {
+        aid: that.data.aid,
+        dataList: that.data.regStu
+      },
+      success(res) {
+        console.log("保存成功", res)
+        that.getFileUrl(res.result.fileID);
+      },
+      fail(res) {
+        console.log("保存失败", res)
+      }
+    })
+  },
+  getFileUrl(fileID) {
+    let that = this;
+    wx.cloud.getTempFileURL({
+      fileList: [fileID],
+      success: res => {
+        // get temp file URL
+        console.log("文件下载链接", res.fileList[0].tempFileURL)
+        that.setData({
+          fileUrl: res.fileList[0].tempFileURL
+        });
+        that.copyFileUrl();
+      },
+      fail: err => {
+        // handle error
+      }
+    })
+  },
+  copyFileUrl() {
+    let that = this
+    wx.setClipboardData({
+      data: that.data.fileUrl,
+      success(res) {
+        wx.getClipboardData({
+          success(res) {
+            console.log("复制成功", res.data) // data
+          }
+        });
+        wx.hideLoading();
+        wx.showToast({
+          title: '已复制下载链接',
+          icon: 'success',
+          duration: 2000
+        });
+      }
+    })
   },
 })
