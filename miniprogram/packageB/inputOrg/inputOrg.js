910fc7d638ac52386cf11928900b56205d7f9023
Li Boyan <lbytony@126.com>
2020-10-26 20:41:51 +0800

    添加inputOrg订阅消息

--- miniprogram/packageB/inputOrg/inputOrg.js
+++ miniprogram/packageB/inputOrg/inputOrg.js
@@ -9,7 +9,6 @@ const db = wx.cloud.database({
 const user = db.collection('user')
 
 Page({
-
   data: {
     openid: '',
     org: ''
@@ -24,30 +23,86 @@ Page({
   onChangeOrg(e) {
     console.log(e)
     this.setData({
-      org : e.detail
+      org: e.detail
     })
   },
 
-  submit(e){
+  submit(e) {
+    let that = this;
     wx.showLoading({
       title: '提交中',
     })
-    user.where({
-      openid : openid
-    })
-    .update({
-      data:{
-        org: org,
-        role: 2
+    const lessonTmplId = ['KWaC5p8fORBoPKNS04b__FdXVoQwLXcVc1R6NLP38o0']
+    wx.requestSubscribeMessage({
+      // 传入订阅消息的模板id，模板 id 可在小程序管理后台申请
+      tmplIds: lessonTmplId,
+      success(res) {
+        console.log(res);
+        // 申请订阅成功
+        if (res.errMsg === 'requestSubscribeMessage:ok') {
+          // 这里将订阅的课程信息调用云函数存入云开发数据
+          new Promise((resolve, reject) => {
+            wx.cloud.callFunction({
+              name: 'watchAudit',
+              data: {
+                openid: that.data.openid,
+                data: {
+                  phrase1: {
+                    value: '通过'
+                  },
+                  thing2: {
+                    value: that.data.org
+                  },
+                  date3: {
+                    value: ''
+                  },
+                },
+                date: new Date(),
+                templateId: lessonTmplId[0],
+              }
+            }).then(() => {
+              resolve();
+            })
+          }).then(() => {
+            user.where({
+              openid: that.data.openid
+            }).update({
+              data: {
+                org: that.data.org,
+                role: 2
+              },
+              success: function (res) {
+                console.log("已成功将角色置2 并记录其部门信息")
+                wx.hideLoading();
+                wx.showToast({
+                  title: '已成功提交申请',
+                  icon: 'none',
+                  duration: 2000
+                })
+                setTimeout(() => {
+                  wx.navigateBack({
+                    delta: 1,
+                  })
+                }, 2000)
+              }
+            })
+          })
+        } else {
+          wx.showToast({
+            title: '提交失败',
+            icon: 'cancel',
+            duration: 2000,
+          });
+        }
       },
-      success: function(res){
-        console.log("已成功将角色置2 并记录其部门信息")
-        wx.hideLoading()
-        wx.navigateBack({
-          delta: 1,
-        })
+      fail: function(err) {
+        wx.showToast({
+          title: '提交失败',
+          icon: 'cancel',
+          duration: 2000,
+        });
+        console.log(err);
       }
-    })
+    });
   }
-
 })
