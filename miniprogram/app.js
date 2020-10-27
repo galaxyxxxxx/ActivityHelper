3e9bae2c660e086d9f2c4ea28046a7b686261d43
x <galaxyx99@163.com>
2020-10-27 15:42:50 +0800

    在加载小程序时  获取用户信息并更新数据库 并加到缓存

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,3 @@
-
 App({
   globalData: {
     openid: '',
@@ -18,39 +17,92 @@ App({
     }
   },
 
-  // 获取openid
+  // get openid ; update and set Storage user's info
   getopenid: function (cb) {
-    if (this.globalData.openid) {
-      typeof cb == "function" && cb(this.globalData.openid)
-    } else {
-      // 调用云函数 获取openid
-      var that = this
-      wx.cloud.callFunction({
-        name: 'login',
-        data: {},
-        success: res => {
-          //闭包函数内，可以用this,而不需要用that=this
-          that.globalData.openid = res.result.openid
-          // openid存入缓存 可以用7000s
-          wx.setStorageSync('openid', res.result.openid)
+
+    var that = this
+
+    // get openid by cloud func
+    const promiseGetOpenid = function () {
+      return new Promise((resolve, reject) => {
+        
+        if (that.globalData.openid) {
           typeof cb == "function" && cb(that.globalData.openid)
-        },
-        fail: err => {
-          wx.showToast({
-            icon: 'none',
-            title: '获取 openid 失败，请检查 login 云函数',
+        } else {
+          // 调用云函数 获取openid
+          wx.cloud.callFunction({
+            name: 'login',
+            data: {},
+            success: res => {
+              //闭包函数内，可以用this,而不需要用that=this
+              that.globalData.openid = res.result.openid
+              // openid存入缓存 可以用7000s
+              wx.setStorageSync('openid', res.result.openid)
+              typeof cb == "function" && cb(that.globalData.openid)
+              resolve()
+            },
+            fail: err => {
+              wx.showToast({
+                icon: 'none',
+                title: '获取 openid 失败，请检查 login 云函数',
+              })
+              console.log('[云函数] [login] 获取 openid 失败，请检查是否有部署云函数，错误信息：', err)
+              reject()
+            },
           })
-          console.log('[云函数] [login] 获取 openid 失败，请检查是否有部署云函数，错误信息：', err)
-        },
+        }
       })
     }
 
-    setTimeout(() => {
-      console.log("hi it's openid", that.globalData.openid)
+    promiseGetOpenid().then(suc => {
+      // 更新user表里的昵称和头像 并加入缓存
+      wx.getUserInfo({
+        success(res) {
+          var userInfo = res.userInfo
+          var nickName = userInfo.nickName
+          var avatarUrl = userInfo.avatarUrl
+          var gender = userInfo.gender
+          var province = userInfo.province
+          var city = userInfo.city
+          var country = userInfo.country
+
+          wx.setStorageSync('nickname', nickName)
+          wx.setStorageSync('avatarUrl', avatarUrl)
+          wx.setStorageSync('gender', gender)
+
+          const db = wx.cloud.database({
+            env: 'x1-vgiba'
+          })
+          const _ = db.command
+
+          wx.cloud.database({
+            env: 'x1-vgiba'
+          }).collection('user').where({
+            _openid: wx.getStorageSync('openid')
+          }).update({
+            data: {
+              nickName: _.set(nickName),
+              avatarUrl: _.set(avatarUrl),
+              gender: _.set(gender),
+              province: _.set(province),
+              city: _.set(city),
+              country: _.set(country)
+            },
+            success(res) {
+              console.log("成功更新用户个人信息", res)
+            },
+            fail(err) {
+              console.log("更新用户个人信息失败", err)
+            }
+          })
+        }
+      })
+
+      // 查询用户角色和手机号 加入缓存
       wx.cloud.database({
         env: 'x1-vgiba'
       }).collection('user').where({
-        _openid: that.globalData.openid
+        _openid: wx.getStorageSync('openid')
       }).get({
         success(res) {
           // role记入缓存
@@ -60,7 +112,9 @@ App({
           wx.setStorageSync('tel', res.data[0].tel)
         }
       })
-    }, 2500);
+    }, err => {
+      console.log('[云函数] [login] 获取 openid 失败2，请检查是否有部署云函数，错误信息：', err)
+    });
   },
 
   getAllPlace() {
@@ -94,5 +148,5 @@ App({
           wx.setStorageSync('allType', type)
         })
     }
-  }
+  },
 })
